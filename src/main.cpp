875e4e297e861e85583e00fdcbaf11e0c97356a6
José Arthur Ferreira Cardoso <josearthurferreiracardoso@gmail.com>
2026-08-17 14:31:40 -0300

    fix: reescrever main.cpp para testar o carregamento do ecs e json

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
+#include <iostream>
+#include <entt/entt.hpp>
+#include "loader.h"
 #include "battle.h"
+#include "components.h"
 
-int main(int argc, char const *argv[]) {
-  startBattle();
-  return 0;
+int main() {
+    entt::registry registry;
+
+    LoadMonsters(registry, "../data/monsters.json");
+
+    BattleManager battleManager(registry);
+
+    auto turnOrder = battleManager.GetTurnOrder();
+    
+    std::cout << "Ordem de Turnos Carregada:\n";
+    for (auto entity : turnOrder) {
+        auto& name = registry.get<NameComponent>(entity).name;
+        auto& combatState = registry.get<CombatStateComponent>(entity);
+        std::cout << "- " << name << " (HP: " << combatState.currentHP << ")\n";
+    }
+
+    return 0;
 }
